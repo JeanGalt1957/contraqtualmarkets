04365c34d376b7264d4de5bb40242bcc21f96c8e
Matthew Gilbertson <matthew.f.gilbertson@gmail.com>
2022-01-16 17:02:21 -0500

    Updates

---
+++ precache-manifest.e03a634ab796f2a81171d64a13503a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7013b052c86aa221fd3d",
+    "url": "/contraqtualmarkets/static/css/main.bfee338f.chunk.css"
+  },
+  {
+    "revision": "7013b052c86aa221fd3d",
+    "url": "/contraqtualmarkets/static/js/main.7013b052.chunk.js"
+  },
+  {
+    "revision": "ad9d849cffe9a6b249da",
+    "url": "/contraqtualmarkets/static/css/1.a36171db.chunk.css"
+  },
+  {
+    "revision": "ad9d849cffe9a6b249da",
+    "url": "/contraqtualmarkets/static/js/1.ad9d849c.chunk.js"
+  },
+  {
+    "revision": "0f1766581f34f7459c23",
+    "url": "/contraqtualmarkets/static/js/runtime~main.0f176658.js"
+  },
+  {
+    "revision": "3a1c7b82c6bad0b4c094b6d1a760bdec",
+    "url": "/contraqtualmarkets/index.html"
+  }
+];
