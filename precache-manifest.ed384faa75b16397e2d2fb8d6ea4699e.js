4b3938829cbfb936c3bf097186948effd20719db
Matthew Gilbertson <matthew.f.gilbertson@gmail.com>
2022-01-21 01:00:04 -0500

    Updates

---
+++ precache-manifest.ed384faa75b16397e2d2fb8d6ea4699e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc98002084a71d8c6ab7",
+    "url": "/contraqtualmarkets/static/css/main.c271fbef.chunk.css"
+  },
+  {
+    "revision": "cc98002084a71d8c6ab7",
+    "url": "/contraqtualmarkets/static/js/main.cc980020.chunk.js"
+  },
+  {
+    "revision": "f470a3cbc47c90d05232",
+    "url": "/contraqtualmarkets/static/css/1.3c7d5091.chunk.css"
+  },
+  {
+    "revision": "f470a3cbc47c90d05232",
+    "url": "/contraqtualmarkets/static/js/1.f470a3cb.chunk.js"
+  },
+  {
+    "revision": "0f1766581f34f7459c23",
+    "url": "/contraqtualmarkets/static/js/runtime~main.0f176658.js"
+  },
+  {
+    "revision": "e180254976ed1c1de8c29bbb0f94a4dc",
+    "url": "/contraqtualmarkets/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/contraqtualmarkets/precache-manifest.e589f3f66ef95982b95525ede89e003f.js"
+  "/contraqtualmarkets/precache-manifest.ed384faa75b16397e2d2fb8d6ea4699e.js"
 );
 
 workbox.clientsClaim();
