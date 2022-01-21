8954792129bf83606040b0b60b8aace14a14cd3f
Matthew Gilbertson <matthew.f.gilbertson@gmail.com>
2022-01-21 00:51:26 -0500

    Fixes navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,11 +10,11 @@ class Menu extends Component {
   render() {
     return (
 		<Navbar bg='dark' variant="dark" expand='lg' fixed="top">
-		<Navbar.Brand href="/">Contraqtual</Navbar.Brand>
+		<Navbar.Brand href="https://jeangalt1957.github.io/contraqtualmarkets/">Contraqtual</Navbar.Brand>
 		<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 		<Navbar.Collapse id="basic-navbar-nav">
 		<Nav className='mx-auto'>
-    <Nav.Link id="homeNav" className="small px-5" href="/">Home</Nav.Link>
+    <Nav.Link id="homeNav" className="small px-5" href="https://jeangalt1957.github.io/contraqtualmarkets/">Home</Nav.Link>
       <Nav.Link id="aboutNav" className="small px-5" href="/about">About</Nav.Link>
 			<Nav.Link id="chatNav" className="small px-5" href="https://jeangalt1957.github.io/DUsocial/" target="_blank">Chat!</Nav.Link>
 			<Nav.Link id="verifiedNav" className="small px-5" href="/verified">Verified Contract</Nav.Link>
