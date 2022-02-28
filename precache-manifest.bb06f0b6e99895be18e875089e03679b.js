6423672f3721e169340de48a618d0ecffb7dac2a
Matthew Gilbertson <matthew.f.gilbertson@gmail.com>
2022-02-27 22:12:52 -0500

    Updates

--- precache-manifest.bb06f0b6e99895be18e875089e03679b.js
+++ precache-manifest.bb06f0b6e99895be18e875089e03679b.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "33782c359e552d6fb9b4",
+    "revision": "2e97290b0b589049e1e1",
     "url": "/contraqtualmarkets/static/css/main.d0117976.chunk.css"
   },
   {
-    "revision": "33782c359e552d6fb9b4",
-    "url": "/contraqtualmarkets/static/js/main.33782c35.chunk.js"
+    "revision": "2e97290b0b589049e1e1",
+    "url": "/contraqtualmarkets/static/js/main.2e97290b.chunk.js"
   },
   {
-    "revision": "dc57f10821b80e468ab7",
+    "revision": "23a7bcfc9b40f1fc3616",
     "url": "/contraqtualmarkets/static/css/1.d3ecda0f.chunk.css"
   },
   {
-    "revision": "dc57f10821b80e468ab7",
-    "url": "/contraqtualmarkets/static/js/1.dc57f108.chunk.js"
+    "revision": "23a7bcfc9b40f1fc3616",
+    "url": "/contraqtualmarkets/static/js/1.23a7bcfc.chunk.js"
   },
   {
     "revision": "0f1766581f34f7459c23",
     "url": "/contraqtualmarkets/static/js/runtime~main.0f176658.js"
   },
   {
-    "revision": "67457eadcd5c43d409cfba058907c9ec",
+    "revision": "218fa213a818b3df362d61256a77d18c",
     "url": "/contraqtualmarkets/index.html"
   }
 ];
